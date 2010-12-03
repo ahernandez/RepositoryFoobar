1d474063e201dcacebe09baba0680dcf28f9fca8
charlesdg <charles.degroote@gmail.com>
2010-12-03 17:47:50 +0000

    friends modification

--- UI/PhpMeetUs/friendsMeeting.php
+++ UI/PhpMeetUs/friendsMeeting.php
@@ -14,11 +14,10 @@ and open the template in the editor.
     ?>
 
 <?php
-
-require_once './appinclude.php';
 echo "<h1>friends_get</h1>";
 echo "<hr />";
-echo "<pre>";
+
+/*echo "<pre>";
 echo "<h2>Scenario 1: Getting all friends of current user</h2>";
 echo "<h5>Raw output:</h5>";
 print_r($facebook->api_client->friends_get());
@@ -29,6 +28,7 @@ echo "<ul>";
 foreach ($friends as $friend) {	echo "<li><fb:name uid=\"$friend\" useyou=\"false\" /></li>";}
 echo "</ul>";
 echo "</pre>";
+*/
 
 ?>
 </html>
