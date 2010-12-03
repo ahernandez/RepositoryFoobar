93c91111075d1ad1a6c5898056dce1e5eb928be3
charlesdg <charles.degroote@gmail.com>
2010-12-03 17:09:29 +0000

    new file:   UI/PhpMeetUs/appinclude.php 	modified:   UI/PhpMeetUs/friendsMeeting.php 	modified:   UI/PhpMeetUs/index2.php

---
+++ UI/PhpMeetUs/appinclude.php
@@ -0,0 +1,16 @@
+<?php
+require_once ‘./facebook/src/facebook.php’;
+// *** Add your Facebook API Key, Secret Key, and Callback URL here *** 
+$appapikey = 'f0b3470dc1e813fc9fb41a88e536cddd';
+$appsecret = '8fa1aca969079a5a848de895d7cb5ee3';
+$appcallbackurl = 'http://apps.facebook.com/easytomeetus/';
+// Connect to Facebook, retrieve user
+$facebook = new Facebook($appapikey, $appsecret);
+$user = $facebook->require_login();
+// Exception handler for invalid session_keys
+try {	// If app is not added, then attempt to add
+	if (!$facebook->api_client->users_isAppAdded()) {		$facebook->redirect($facebook->get_add_url());	} 
+} catch (Exception $ex) {
+	// Clear out cookies for app and redirect user to a login prompt 
+	$facebook->set_user(null, null);	$facebook->redirect($appcallbackurl);
+}?>
--- UI/PhpMeetUs/friendsMeeting.php
+++ UI/PhpMeetUs/friendsMeeting.php
@@ -13,4 +13,24 @@ and open the template in the editor.
         echo $description;
     ?>
 
+<?php
+echo “<h1>Test</h1>”;
+
+/*
+require_once ‘appinclude.php’;
+echo “<h1>friends_get</h1>”;
+echo “<hr />”;
+echo “<pre>”;
+echo “<h2>Scenario 1: Getting all friends of current user</h2>”;
+echo “<h5>Raw output:</h5>”;
+print_r($facebook->api_client->friends_get());
+echo “<h5>Applied example:</h5>”;
+echo “<p>Friends of <fb:name uid=\”$user\” useyou=\”false\” />:</p>”;
+$friends = $facebook->api_client->friends_get();
+echo “<ul>”;
+foreach ($friends as $friend) {	echo “<li><fb:name uid=\”$friend\” useyou=\”false\” /></li>”;}
+echo “</ul>”;
+echo “</pre>”;
+*/
+?>
 </html>
