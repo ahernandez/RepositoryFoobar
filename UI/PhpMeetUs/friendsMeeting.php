75f76a7335c5c5174793326082423595998b478b
charlesdg <charles.degroote@gmail.com>
2010-12-10 13:43:32 +0000

    friends 1

--- UI/PhpMeetUs/friendsMeeting.php
+++ UI/PhpMeetUs/friendsMeeting.php
@@ -18,4 +18,70 @@ and open the template in the editor.
        
     ?>
 
+<?php
+require '../src/facebook.php';
+
+$facebook = new Facebook(array(
+  'appId'  => '102810506459481',
+  'secret' => '8fa1aca969079a5a848de895d7cb5ee3',
+  'cookie' => false
+));
+
+try {
+  $session = $facebook->getSession();
+  
+  if (!$session) {
+    $url=$facebook->getLoginUrl(array('canvas' => 1,'fbconnect' => 0));
+    echo ('<script type="text/javascript">top.location.href=\''.$url.'\';</script>');
+} 
+else {
+  
+  $me = $facebook->api('/me');  
+   
+  //print_r($me);
+  
+  echo '<p> Hi '. $me['name'] .', Select your friends : </p><br><br>';
+  
+ 
+  $friends = $facebook->api("me/friends");
+  
+  $friends_list = $friends['data'];
+  
+  ?>
+
+  <form action="test.php">
+  <div style="width:300px;height:300px;overflow:auto;"> 
+  <input type="hidden" name="envoi" value="yes"> 
+  <?php
+  foreach ($friends_list as $friend) { 
+  	?>
+  	<input type="checkbox" name="friends_chosen[]" value=<?php echo '"'. $friend['id'] .'"'; ?> >&nbsp; <?php echo $friend['name']; ?><br>
+  	
+  	<?php
+  	} 
+
+?>
+</div>
+<br><br>
+<input type="submit" value="Send invitation to friends">
+</form>
+<?php   
+	$envoi = $_GET['envoi'];
+	$friends_chosen = $_GET['friends_chosen'];
+	
+	if ($envoi == 'yes') {
+		$options_text = implode(', ',$friends_chosen);
+	
+     	echo '<p>friends chosen:<br><br>'.$options_text.'</p>';
+	}
+
+}
+ 
+  
+} catch (FacebookApiException $e) {
+  print 'error: ' . $e;
+  error_log($e);
+}
+?>
+
 </html>
